194f250fc8e89aa3234cd568f6e2a412a7152b68
NuggetBrun <157137637+kazysys@users.noreply.github.com>
2024-10-14 23:22:48 -0300

    Update users.ts

--- pages/api/users.ts
+++ pages/api/users.ts
@@ -1,4 +1,5 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
+// pages/api/users.ts
+import { NextApiRequest, NextApiResponse } from 'next';
 import dbConnect from '../../lib/dbConnect';
 import User from '../../models/User';
 
@@ -6,19 +7,22 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   await dbConnect();
 
   if (req.method === 'POST') {
-    const { username } = req.body;
-
     try {
-      const user = await User.create({ username });
+      const user = await User.create(req.body);
       res.status(201).json(user);
     } catch (error) {
-      res.status(400).json({ message: error.message });
+      // Verifica se o erro é uma instância de Error
+      if (error instanceof Error) {
+        res.status(400).json({ message: error.message });
+      } else {
+        res.status(400).json({ message: 'Erro desconhecido' });
+      }
     }
   } else if (req.method === 'GET') {
     const users = await User.find();
     res.status(200).json(users);
   } else {
     res.setHeader('Allow', ['GET', 'POST']);
-    res.status(405).end(`Method ${req.method} Not Allowed`);
+    res.status(405).end(`Método ${req.method} não permitido`);
   }
 }
