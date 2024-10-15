531a73dc5a0fd9c23256d48e33956abb693d0c83
NuggetBrun <157137637+kazysys@users.noreply.github.com>
2024-10-14 23:14:58 -0300

    Update dbConnect.ts

--- lib/dbConnect.ts
+++ lib/dbConnect.ts
@@ -2,23 +2,25 @@
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 
-dotenv.config(); // Adicione esta linha para carregar as variáveis de ambiente
+dotenv.config(); // Carrega as variáveis de ambiente do arquivo .env
 
 const dbConnect = async () => {
+  // Verifica se a conexão já está estabelecida
   if (mongoose.connection.readyState === 1) {
     return;
   }
 
   const uri = process.env.MONGODB_URI; // Acessando a variável de ambiente
 
+  // Verifica se a URI do MongoDB está definida
   if (!uri) {
     throw new Error('A string de conexão do MongoDB não está definida.');
   }
 
   try {
+    // Conecta ao MongoDB
     await mongoose.connect(uri, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
+      // As opções useNewUrlParser e useUnifiedTopology foram removidas
     });
   } catch (error) {
     console.error('Erro ao conectar ao MongoDB:', error);
